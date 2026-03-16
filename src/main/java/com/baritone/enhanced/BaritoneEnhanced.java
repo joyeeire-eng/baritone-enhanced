f42e616a228b9f44ce6f23fa650a0729e8ed0c28
Joyee Ye <joyee@Joyees-MacBook-Pro-2020.local>
2026-03-16 10:08:47 +0000

    Ultra-minimal mod class - just basic @Mod annotation

--- src/main/java/com/baritone/enhanced/BaritoneEnhanced.java
+++ src/main/java/com/baritone/enhanced/BaritoneEnhanced.java
@@ -1,27 +1,12 @@
 package com.baritone.enhanced;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Mod(BaritoneEnhanced.MOD_ID)
 public class BaritoneEnhanced {
     public static final String MOD_ID = "baritoneenhanced";
-    private static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+    
     public BaritoneEnhanced() {
-        // Register the setup method for mod loading
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(this::clientSetup);
-        
-        LOGGER.info("Baritone Enhanced initialized!");
-    }
-
-    private void clientSetup(final FMLClientSetupEvent event) {
-        LOGGER.info("Baritone Enhanced client setup complete");
+        System.out.println("Baritone Enhanced mod loaded!");
     }
 }
