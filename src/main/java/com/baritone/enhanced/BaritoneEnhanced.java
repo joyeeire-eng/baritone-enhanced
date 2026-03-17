c4054283376980769f773eb57619c9fe3a49d527
Joyee Ye <joyee@Joyees-MacBook-Pro-2020.local>
2026-03-17 09:31:13 +0000

    Simple Java build - no Forge dependencies

--- src/main/java/com/baritone/enhanced/BaritoneEnhanced.java
+++ src/main/java/com/baritone/enhanced/BaritoneEnhanced.java
@@ -1,8 +1,5 @@
 package com.baritone.enhanced;
 
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(BaritoneEnhanced.MOD_ID)
 public class BaritoneEnhanced {
     public static final String MOD_ID = "baritoneenhanced";
     
